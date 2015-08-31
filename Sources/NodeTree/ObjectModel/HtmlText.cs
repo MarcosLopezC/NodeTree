8fe10b9b1347a3f57af98ba4fc26f152a04e5202
MarcosLopezC <MarcosLopezC@live.com>
2015-08-31 14:46:30 -0400

    Saving work.

--- Sources/NodeTree/ObjectModel/HtmlText.cs
+++ Sources/NodeTree/ObjectModel/HtmlText.cs
@@ -6,9 +6,26 @@ namespace NodeTree.ObjectModel
 {
 	public class HtmlText : IHtmlRenderable
 	{
+		public string Text { get; private set; }
+
+		public HtmlText(string text)
+		{
+			this.Text = text;
+		}
+
 		public void Render(HtmlWriter writer)
 		{
-			throw new NotImplementedException();
+			writer.WriteEncoded(this.Text);
+		}
+
+		public override string ToString()
+		{
+			return this.Text;
+		}
+
+		public static implicit operator HtmlText(string text)
+		{
+			return new HtmlText(text);
 		}
 	}
 }
