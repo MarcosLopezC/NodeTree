234752c2018dcd15ca6e5540a061aa4ee6b9b4a0
Marcos López C <MarcosLopezC@live.com>
2015-09-15 17:40:02 -0400

    Saving work.

--- Sources/NodeTree/ObjectModel/HtmlComment.cs
+++ Sources/NodeTree/ObjectModel/HtmlComment.cs
@@ -15,14 +15,33 @@ namespace NodeTree.ObjectModel
 
 		public void Render(HtmlWriter writer)
 		{
-			if (!this.Text.StartsWith(">") &&
-				!this.Text.StartsWith("->") &&
-				!this.Text.Contains("--"))
+			writer.Write("<!-- ");
+
+			var start = 0;
+
+			if (this.Text.StartsWith(">"))
 			{
-				writer.Write("<!-- ");
-				writer.Write(this.Text);
-				writer.Write(" -->");
+				start = 1;
 			}
+			else if (this.Text.StartsWith("->"))
+			{
+				start = 2;
+			}
+
+			if (this.Text[start] != '-')
+			{
+				writer.Write(this.Text[start]);
+			}
+
+			for (var i = start + 1; i < this.Text.Length; i += 1)
+			{
+				if (this.Text[i] != '-' || this.Text[i - 1] != '-')
+				{
+					writer.Write(this.Text[i]);
+				}
+			}
+
+			writer.WriteLine(" -->");
 		}
 
 		public override string ToString()
